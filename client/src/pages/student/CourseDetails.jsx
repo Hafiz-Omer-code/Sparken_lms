fb4f8ebf9860db1e6f26d0403d83bf56e4d11eb3
Hafiz Omer Bin Ahmad <Omer6688660@gmail.com>
2025-03-12 16:00:08 +0500

    Auto-update: 2025-03-12 16:00:08

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -7,7 +7,7 @@ import Loading from "../../components/student/Loading";
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setcourseData] = useState(null);
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating } = useContext(AppContext);
   const fetchCourseData = async () => {
     const findCourse = allCourses.find((course) => course._id === id);
     setcourseData(findCourse);
@@ -15,24 +15,55 @@ const CourseDetails = () => {
 
   useEffect(() => {
     fetchCourseData();
-  },[]);
+  }, []);
 
   return courseData ? (
     <>
       <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
-
         <div className="absolute top-0 left-0 w-full h-section-height -z-1 hg-gradient-to-b from cyan-100/70"></div>
-      
-      {/* left column */}
-      <div>
-        <h1>{courseData.courseTitle}</h1>
-        <p>{courseData.courseDescription}</p>
+
+        {/* left column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200) + "...",
+            }}
+          ></p>
+
+          {/* {review and rating} */}
+          <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-blue-600">({courseData.courseRatings.length}{courseData.courseRatings.length > 1 ? " ratings" : " rating"})</p>
+
+            <p>{courseData.enrolledStudents.length}{courseData.enrolledStudents.length > 1 ? " students enrolled" : " student enrolled"}</p>
+          </div>
+          <p>Course by <span className="text-sm">Sparken</span></p>
+        </div>
+        {/* right column */}
+        <div></div>
       </div>
-      {/* right column */}
-      <div></div>
-    </div>
     </>
-  ) : <Loading />
-}
+  ) : (
+    <Loading />
+  );
+};
 
 export default CourseDetails;
