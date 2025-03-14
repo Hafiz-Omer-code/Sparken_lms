8434a70cc97f177d47107a55e3a8be5dca9b9a14
Hafiz Omer Bin Ahmad <Omer6688660@gmail.com>
2025-03-14 12:00:11 +0500

    Auto-update: 2025-03-14 12:00:11

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { dummyEducatorData } from '../../assets/assets'
+import { UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData;
+  const {user} = useUser();
   return (
     <div>
-        <h1>Edu Navbar</h1>
+        
     </div>
   )
 }
--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -7,6 +7,7 @@ import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
 import { useState, useEffect } from 'react'
 import YouTube from 'react-youtube'
+import Rating from '../../components/student/Rating'
 
 const Player = () => {
   const {enrolledCourses, calculateChapterTime,} = useContext(AppContext)
@@ -81,6 +82,7 @@ const Player = () => {
             </div>
             <div className='flex items-center gap-2 py-3 mt-10'>
               <h1 className='text-xl font-semibold'>Rate this Course:</h1>
+              <Rating initialRating={0}/>
             </div>
         </div>
         {/* {right column } */}
